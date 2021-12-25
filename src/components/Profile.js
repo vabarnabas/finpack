ce09957362ba5cbd786e223f889f7b46ba05f718
vabarnabas <vabarnabas@gmail.com>
2021-12-25 15:09:47 +0100

    📄 Small Update

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { HiX } from 'react-icons/hi'
+import { HiX, HiFolder } from 'react-icons/hi'
 
 const Profile = (props) => {
     return (
         <div className='dashboard-card group'>
-            <HiX onClick={() => {localStorage.removeItem(props.position);sessionStorage.removeItem(props.position);props.setStateChange(props.stateChange+1)}} className='absolute top-3 right-3 text-slate-500 dark:text-slate-500 hover:text-slate-600 dark:hover:text-slate-600 text-lg'/>
+            <div className="absolute w-full top-3 px-3 flex items-center justify-center">
+                <p className="text-xs font-semibold mr-auto text-slate-500 pl-2">PROFILOM</p>
+                <div className="relative ml-auto flex justify-center items-center space-x-2">
+                <HiFolder className='text-slate-500 dark:text-slate-500 hover:text-slate-600 dark:hover:text-slate-600'/>
+                <HiX onClick={() => {localStorage.removeItem(props.position);sessionStorage.removeItem(props.position);props.setStateChange(props.stateChange+1)}} className='ml-auto text-slate-500 dark:text-slate-500 hover:text-slate-600 dark:hover:text-slate-600 text-lg'/>
+                </div>
+            </div>
         </div>
     )
 }
